29b90038774a8b043b714c0974977c8b74f3051b
Hassan Mian <mian@willandskill.se>
2021-01-07 12:06:06 +0100

    Post credentials

--- src/App.js
+++ src/App.js
@@ -15,9 +15,27 @@ function App() {
     setFormData(newObj)
   }
 
+  function handleOnSubmit(e) {
+    e.preventDefault()
+    const url = "https://frebi.willandskill.eu/api-token-auth/"
+    const payload = {
+      email: formData.email,
+      password: formData.password
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleOnSubmit}>
         <label>Email</label>
         <input name="email" value={formData.email} onChange={handleOnChange}/>
         <label>Password</label>
