cdb6ee83fa828ce25682b53849dae235b69de597
Hassan Mian <mian@willandskill.se>
2021-01-07 11:13:02 +0100

    Use one state to handle all input data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, {useState} from 'react'
 
 function App() {
-
   const [emailInput, setEmailInput] = useState("")
   const [passwordInput, setPasswordInput] = useState("")
+  const [formData, setFormData] = useState({
+    email: "webb19@willandskill.se",
+    password: "javascriptoramverk"
+  })
 
-  function handleEmailInput(e) {
-    setEmailInput(e.target.value)
-  }
-
-  function handlePasswordInput(e) {
-    setPasswordInput(e.target.value)
+  function handleOnChange(e) {
+    const inputName = e.target.name
+    const inputValue = e.target.value
+    const newObj = {...formData, [inputName]: inputValue}
+    setFormData(newObj)
   }
 
   return (
     <div>
       <form>
         <label>Email</label>
-        <input onChange={handleEmailInput}/>
+        <input name="email" value={formData.email} onChange={handleOnChange}/>
         <label>Password</label>
-        <input onChange={handlePasswordInput}/>
+        <input name="password" value={formData.password} onChange={handleOnChange}/>
         <button type="submit">Log In</button>
       </form>
     </div>
